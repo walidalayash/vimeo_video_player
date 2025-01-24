9112dec8cca228edb2e0eb2f2ec238d1bc46ba16
Walid Alayash <walidalayash99@gmail.com>
2025-01-24 09:45:59 +0200

    test

--- lib/src/vimeo_player.dart
+++ lib/src/vimeo_player.dart
@@ -117,13 +117,6 @@ class VimeoVideoPlayer extends StatelessWidget {
         mediaPlaybackRequiresUserGesture: false,
         allowsInlineMediaPlayback: true,
         useHybridComposition: true,
-        javaScriptEnabled: true, 
-        javaScriptCanOpenWindowsAutomatically: true,
-        allowUniversalAccessFromFileURLs: true, 
-        clearCache: true,
-        userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/537.36 (KHTML, like Gecko) Version/14.0 Mobile Safari/537.36",
-
-
       ),
       initialData: InAppWebViewInitialData(
         data: _buildHtmlContent(),
@@ -198,14 +191,8 @@ class VimeoVideoPlayer extends StatelessWidget {
 
   /// Builds the iframe URL
   String _buildIframeUrl() {
-    return 'https://player.vimeo.com/video/$videoId'
-        'autoplay=$isAutoPlay'
-        '&loop=$isLooping'
-        '&muted=$isMuted'
-        '&title=$showTitle'
-        '&byline=$showByline'
-        '&controls=$showControls'
-        '&dnt=$enableDNT';
+    return 'https://player.vimeo.com/video/1048645456?h=57021fbf87';
+       
   }
 
   /// Manage vimeo player events received from the WebView
